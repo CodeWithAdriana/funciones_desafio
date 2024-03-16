a54b57c6232e44029b9a204ee9a0d92635df99ea
Adri <adrianareyesgarcia1@gmail.com>
2024-03-15 20:46:21 -0500

    primeros cambios

---
+++ asset/Js/1_funcion.js
@@ -0,0 +1,11 @@
+function example(a, b, c){
+    return a+b+c;
+}
+
+//conversion a una funcion de expresion
+
+const example = function(a, b, c) {
+    return a + b + c;
+};
+
+
---
+++ asset/Js/2_arrow.js
@@ -0,0 +1,7 @@
+let suma = function(a, b){
+    return a + b
+}
+
+//cambiando a arrow function de una linea. 
+
+let suma = (a, b) => a + b;
